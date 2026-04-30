a58dc8478b15a0680899a83db1b22b0ca8e97c9f
Rituja-454 <ritsssonawane42@gmail.com>
2026-04-30 15:43:19 +0530

    authentication API backend routing

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const app=express();
 const PORT = process.env.PORT;
 connectDB();
 
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const authRoute = require("./routes/authRoutes");
+app.use("./api/auth", authRoute);
+
 app.listen(PORT ,(req,res)=>{
   console.log(`server start on PORT ${PORT}`);
 })
