333c838b8e9c7d67b1dd5c2aa544bf8440fe19be
Rituja-454 <ritsssonawane42@gmail.com>
2026-04-30 14:13:48 +0530

    user authentication APIs created

---
+++ backend/controllers/authController.js
@@ -0,0 +1,100 @@
+const User = require ("./models/User.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function JWTtokenGeneration(user){
+   jwt.sign(
+     {
+      id:user._id,
+      role:user.role
+     },
+     process.env.JWT_SECRET,
+     {
+      expiresIn:"1h"
+     }
+
+   )
+}
+
+async function registerUser (req,res){
+  const {name,email,password}= req.body;
+  try{
+      if(!name || !email || !password){
+    return res.json({message:"All fields must be required"})
+    console.log ("all fields must be required");
+    alert ("all fields required");
+  }
+  
+  const existingUser = await User.findOne({email});
+  if(existingUser){
+    return res.status(400).json({message:"User already exist"})
+    console.log("user already exist");
+    alert("registration failed")
+  }
+
+ const salt =await bcrypt.genSalt(10);
+ const hashedPW = await bcrypt.hash(password,salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password:hashedPW ,
+    role:"employee"
+  });
+
+  res.status(201).json(
+    {message:"user registered successfully" ,
+      user:{
+            id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role
+      },
+      token:JWTtokenGeneration(user)
+    });
+
+  }
+  catch(error){
+    console.log(error);
+    res.status(500).json({message:"server error"})
+  }
+}
+
+const loginUser = async(req,res) =>{
+  try{
+      const user=req.body;
+      if(!user.email || !user.password){
+        return res.json({message:"must be filled required information"})
+      }
+
+      const user =await User.findOne({email:user.email});   // complete user data will load in "user"
+      if(!user){
+        return res.json({message:"user not found"});
+      }
+
+      const pwMatch =await bcrypt.compare(user.password , password)
+      if(!pwMatch){
+        return res.json({message:"invalid password"})
+      }
+
+      res.json({message:"user login successfully",
+        user:{
+          id:user._id,
+          name:user.name,
+          email:user.email,
+          role:user.role
+        },
+        token:JWTtokenGeneration(user)
+      }
+      )
+  }
+  catch(error){
+     console.log(error);
+     res.json({message:"server error"});
+  }
+}
+
+module.exports ={
+  registerUser ,
+  loginUser
+}
